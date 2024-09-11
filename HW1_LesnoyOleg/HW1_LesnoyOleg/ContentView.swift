8b24efc88290baac7a0a38734510ba9ec7eccf74
Oleg Lesnoy <oleglv98@gmail.com>
2024-09-11 16:34:41 +0200

    create HW1_LesnoyOleg project

---
+++ HW1_LesnoyOleg/HW1_LesnoyOleg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW1_LesnoyOleg
+//
+//  Created by Oleg Lesnoy on 11.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW1_LesnoyOleg/HW1_LesnoyOleg/HW1_LesnoyOlegApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_LesnoyOlegApp.swift
+//  HW1_LesnoyOleg
+//
+//  Created by Oleg Lesnoy on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW1_LesnoyOlegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
